7c29a78ab907cf09878c19ee8659f77c62129396
Kansei <linkan@gmx.de>
2017-03-04 16:22:58 +0100

    rebuilt generator - change from spring to Kafka Java API

--- generator/src/main/java/de/hska/bdelab/Generator.java
+++ generator/src/main/java/de/hska/bdelab/Generator.java
@@ -1,17 +1,19 @@
 package de.hska.bdelab;
 
 import java.util.Date;
+import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.logging.Logger;
 
-import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.messaging.Source;
-import org.springframework.integration.annotation.InboundChannelAdapter;
-import org.springframework.integration.annotation.Poller;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
-@EnableBinding(Source.class)
-public class Generator {
-	
+public class Generator
+{
+	private static final Logger LOGGER = Logger.getLogger( Generator.class.getName() );
+
 	final String[] urls = {
 			"index",
 			"start",
@@ -30,28 +32,43 @@ public class Generator {
 			"help"
 	};
 
+    public void Run() {
+    	LOGGER.info("generator startup.");
 
-	public Generator(){
+        Properties props = new Properties();
+        props.put("bootstrap.servers", "localhost:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("batch.size", 16384);
+        props.put("linger.ms", 1);
+        props.put("buffer.memory", 33554432);
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-	}
-	
-	@InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "5000", maxMessagesPerPoll = "1"))
-	public String pageviewMessageSource()  {		
-		return (GenerateTimestamp() + "," + GenerateIp() + "," + GenerateUri() + "," + GenerateUid());
-	}
-	
-	/*
-	@Bean
-	@InboundChannelAdapter(value = Source.OUTPUT, poller = @Poller(fixedDelay = "5000", maxMessagesPerPoll = "1"))
-	public MessageSource<String> pageviewMessageSource()  {
-		// create ViewObject with "randomvalues"
-		ViewObject vo = new ViewObject(GenerateTimestamp(),
-				GenerateIp(),
-				GenerateUri(),
-				GenerateUid());
-		
-		return() -> new GenericMessage<String>(vo.getTimestamp() + "," + vo.getIp() + "," + vo.getUri() + "," + vo.getUid());
-	}*/
+
+        // send(topic, key, value)
+        Producer<String, String> producer = new KafkaProducer(props);
+
+        try {
+        	for(;;) {
+            	producer.send(new ProducerRecord<String, String>("output", "pageviews", NewMessage()));
+            	try {
+            		Thread.sleep(ThreadLocalRandom.current().nextInt(100,5000));
+            	} catch(InterruptedException ex) {
+            		ex.printStackTrace();
+            	}
+            }
+        } catch (Exception ex) {
+        	LOGGER.warning(ex.getMessage());
+        } finally {
+        	producer.close();
+        	LOGGER.info("generator shutown.");
+        }
+    }
+    
+    private String NewMessage() {
+    	return(GenerateTimestamp() + "," + GenerateUri() + "," + GenerateIp() + "," + GenerateUid());
+    }
 
 	// rnd element generators
 	private String GenerateTimestamp() {
@@ -66,12 +83,10 @@ public class Generator {
 	private String GenerateUid() {
 		return (UUID.randomUUID().toString());
 	}
-
+	
+	// helpers
 	private String rndDecByte() {
 		return String.valueOf(ThreadLocalRandom.current().nextInt(0, 256));
 	}
-
-
-
 
 }
---
+++ generator/src/main/java/de/hska/bdelab/GeneratorApp.java
@@ -0,0 +1,10 @@
+package de.hska.bdelab;
+
+
+public class GeneratorApp 
+{
+    public static void main( String[] args )
+    {
+        new Generator().Run();
+    }
+}
