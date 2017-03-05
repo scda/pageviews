429a3bc254e337bbcc7437be54a748f10c58402d
Kansei <linkan@gmx.de>
2017-03-05 22:34:39 +0100

    environment ettings setup modified

--- vm/puppet/modules/hadoop/files/10-hdp-environment.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64
-export HADOOP_CLASSPATH=${JAVA_HOME}/lib/tools.jar
-export PATH=$PATH:$JAVA_HOME/bin
-export HADOOP_HOME=/opt/hadoop-2.7.3
