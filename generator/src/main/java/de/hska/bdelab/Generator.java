f48f7931a5de81504f24be9f1045329d7b6b2a96
Kansei <linkan@gmx.de>
2017-03-05 15:02:36 +0100

    formatting

--- generator/src/main/java/de/hska/bdelab/Generator.java
+++ generator/src/main/java/de/hska/bdelab/Generator.java
@@ -32,43 +32,42 @@ public class Generator
 			"help"
 	};
 
-    public void Run() {
-    	LOGGER.info("generator startup.");
+  public void Run() {
+  	LOGGER.info("generator startup.");
 
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "10.10.33.22:9092");
-        props.put("acks", "all");
-        props.put("retries", 0);
-        props.put("batch.size", 16384);
-        props.put("linger.ms", 1);
-        props.put("buffer.memory", 33554432);
-        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    Properties props = new Properties();
+    props.put("bootstrap.servers", "10.10.33.22:9092");
+    props.put("acks", "all");
+    props.put("retries", 0);
+    props.put("batch.size", 16384);
+    props.put("linger.ms", 1);
+    props.put("buffer.memory", 33554432);
+    props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+    props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
+    Producer<String, String> producer = new KafkaProducer(props);
 
-        // send(topic, key, value)
-        Producer<String, String> producer = new KafkaProducer(props);
-
-        try {
-        	for(;;) {
-            	producer.send(new ProducerRecord<String, String>("output", "pageviews", NewMessage()));
-            	try {
-            		Thread.sleep(ThreadLocalRandom.current().nextInt(100,5000));
-            	} catch(InterruptedException ex) {
-            		ex.printStackTrace();
-            	}
-            }
-        } catch (Exception ex) {
-        	LOGGER.warning(ex.getMessage());
-        } finally {
-        	producer.close();
-        	LOGGER.info("generator shutown.");
-        }
+    try {
+    	for(;;) {
+				// send(topic, key, value)
+      	producer.send(new ProducerRecord<String, String>("output", "pageviews", NewMessage()));
+      	try {
+      		Thread.sleep(ThreadLocalRandom.current().nextInt(100,5000));
+      	} catch(InterruptedException ex) {
+      		ex.printStackTrace();
+      	}
+      }
+    } catch (Exception ex) {
+    	LOGGER.warning(ex.getMessage());
+    } finally {
+    	producer.close();
     }
-    
-    private String NewMessage() {
-    	return(GenerateTimestamp() + "," + GenerateUri() + "," + GenerateIp() + "," + GenerateUid());
-    }
+		LOGGER.info("generator shutdown.");
+  }
+
+  private String NewMessage() {
+  	return(GenerateTimestamp() + "," + GenerateUri() + "," + GenerateIp() + "," + GenerateUid());
+  }
 
 	// rnd element generators
 	private String GenerateTimestamp() {
@@ -83,7 +82,7 @@ public class Generator
 	private String GenerateUid() {
 		return (UUID.randomUUID().toString());
 	}
-	
+
 	// helpers
 	private String rndDecByte() {
 		return String.valueOf(ThreadLocalRandom.current().nextInt(0, 256));
