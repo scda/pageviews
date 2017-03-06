588862d40dade44e7e53a5f8f21d2ee6d8a2333b
Kansei <linkan@gmx.de>
2017-03-06 14:13:04 +0100

    logging to hadoop/logs

--- vm/puppet/modules/hadoop/files/run-batch.sh
+++ vm/puppet/modules/hadoop/files/run-batch.sh
@@ -4,5 +4,4 @@ HOUR=$(date +%H)
 MINUTE=$(date +%M)
 
 cd /opt/hadoop-2.7.3/
-#/opt/hadoop-2.7.3/bin/hdfs dfs -rm -r /output/$DATE/$HOUR/
-bin/hadoop jar pv.jar PageViews /input/$DATE/$HOUR /output/$DATE/$HOUR/$MINUTE
+bin/hadoop jar pv.jar PageViews /input/$DATE/$HOUR /output/$DATE/$HOUR/$MINUTE &> /opt/hadoop-2.7.3/logs/mapreduce-pageviews-$DATE-$HOUR-$MINUTE.log
