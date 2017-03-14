f149df62d2f908068988c23eb853c3e840f5cca0
Kansei <linkan@gmx.de>
2017-03-14 22:06:56 +0100

    added hdfs to cassandra job to run-batch

--- vm/puppet/modules/hadoop/files/run-batch.sh
+++ vm/puppet/modules/hadoop/files/run-batch.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 DATE=$(date +%y-%m-%d)
+DATEMIN=$(date +%Y%m%d)
 HOUR=$(date +%H)
 MINUTE=$(date +%M)
 
 cd /opt/hadoop-2.7.3/
 bin/hdfs dfs -rm -r /output/$DATE/$HOUR
-bin/hadoop jar pv.jar PageViews /input/$DATE/$HOUR /output/$DATE/$HOUR &> /opt/hadoop-2.7.3/logs/mapreduce-pageviews-$DATE-$HOUR-$MINUTE.log
+bin/hadoop jar jobs/pageviewsJob.jar /input/$DATE/$HOUR /output/$DATE/$HOUR &>  /opt/hadoop-2.7.3/logs/mapreduce-pageviews-$DATE-$HOUR-$MINUTE.log
+java -jar /opt/dbwriter/target/pv-db-writer-full.jar /output/$DATE/$HOUR $DATEMIN $HOUR 
