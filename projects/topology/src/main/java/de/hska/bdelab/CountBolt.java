3bbfd1462edc4f888957f40c7ce4bef3555b9b35
Kansei <linkan@gmx.de>
2017-03-16 14:45:51 +0100

    topology writes to dashboard

--- projects/topology/src/main/java/de/hska/bdelab/CountBolt.java
+++ projects/topology/src/main/java/de/hska/bdelab/CountBolt.java
@@ -11,15 +11,22 @@ import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Tuple;
 import org.apache.storm.tuple.Values;
 
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
 public class CountBolt extends BaseRichBolt{
 	private static final long serialVersionUID = 4542793265656293782L;
 	OutputCollector _collector;
 	Map<String, Integer> counts = new HashMap<String, Integer>();
+	CloseableHttpClient _httpClient;
 	
+	HttpPost widgetPost = new HttpPost("http://10.0.2.2:3030/widgets/pageviews");	
 	@Override
 	public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
 		_collector = collector;
-		
+		_httpClient = HttpClients.createDefault();
 	}
 
 	@Override
@@ -30,6 +37,25 @@ public class CountBolt extends BaseRichBolt{
 		count++;
 		counts.put(url, count);
 		_collector.emit(new Values(url, count));
+		
+		updateDash();
+	}
+	
+	private void updateDash() {
+		String postString = new String("{\"auth_token\": \"pageviewskey\", \"items\":[");
+		for (Map.Entry<String, Integer> entry : counts.entrySet()) {
+			postString += "{\"label\":\"" + entry.getKey() + "\", \"value\":\"" + entry.getValue().toString() + "\"},";
+		}
+		postString = postString.substring(0, postString.length()-1) + "]}";
+		
+
+		try {
+			widgetPost.setEntity(new StringEntity(postString));
+			_httpClient.execute(widgetPost);	
+		} catch(Exception ex) {
+			/*System.out.println("send failed.");
+			ex.printStackTrace();*/
+		}
 	}
 
 	@Override
